61da177fc0058d10337557798213449b1b0778b9
Sergey <sergeylabetsik@datateer.com>
2024-12-21 18:36:58 -0600

    Update dependencies

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import React, { Component, Fragment } from 'react';
-import { Route } from 'react-router-dom';
-import { inject, observer } from 'mobx-react';
+import React, { Component } from "react";
+import { Route, Routes } from "react-router-dom";
+import { inject, observer } from "mobx-react";
 
-import SignInPage from './pages/signin/SignInPage';
-import SignUpPage from './pages/signup/SignUpPage';
-import TasksPage from './pages/tasks/TasksPage';
-import CreateTaskPage from './pages/create-task/CreateTaskPage';
+import SignInPage from "./pages/signin/SignInPage";
+import SignUpPage from "./pages/signup/SignUpPage";
+import TasksPage from "./pages/tasks/TasksPage";
+import CreateTaskPage from "./pages/create-task/CreateTaskPage";
 
-@inject('routerStore')
+@inject("routerStore")
 @observer
 class App extends Component {
   render() {
     return (
-      <Fragment>
-        <Route exact path="/" component={SignInPage} />
-        <Route path="/signin/" component={SignInPage} />
-        <Route path="/signup/" component={SignUpPage} />
-        <Route exact path="/tasks" component={TasksPage} />
-        <Route exact path="/tasks/create" component={CreateTaskPage} />
-      </Fragment>
+      <Routes>
+        <Route exact path="/" element={<SignInPage />} />
+        <Route path="/signin/" element={<SignInPage />} />
+        <Route path="/signup/" element={<SignUpPage />} />
+        <Route exact path="/tasks" element={<TasksPage />} />
+        <Route exact path="/tasks/create" element={<CreateTaskPage />} />
+      </Routes>
     );
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,18 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'mobx-react';
-import { createBrowserHistory } from 'history';
-import { RouterStore, syncHistoryWithStore } from 'mobx-react-router';
-import { HashRouter } from 'react-router-dom';
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { Provider } from "mobx-react";
+import { createBrowserHistory } from "history";
+import { RouterStore, syncHistoryWithStore } from "@ibm/mobx-react-router";
+import { HashRouter } from "react-router-dom";
 
-import './index.scss';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-import TasksService from './services/tasks.service';
-import TasksStore from './stores/tasks.store';
-import UserStore from './stores/user.store';
-import AuthService from './services/auth.service';
-
+import "./index.scss";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import TasksService from "./services/tasks.service";
+import TasksStore from "./stores/tasks.store";
+import UserStore from "./stores/user.store";
+import AuthService from "./services/auth.service";
 
 const services = {};
 const stores = {};
@@ -27,15 +26,16 @@ services.authService = new AuthService();
 stores.tasksStore = new TasksStore(services.tasksService);
 stores.userStore = new UserStore(services.authService);
 
-const Root = (
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+
+root.render(
   <Provider {...stores}>
     <HashRouter history={history}>
       <App />
     </HashRouter>
   </Provider>
 );
-ReactDOM.render(Root, document.getElementById('root'));
-
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
